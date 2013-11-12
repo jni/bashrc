f22543770d7d72c5bdb26db3c831e242a6c9460b
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2013-11-12 16:12:04 +1100

    Remove inputrc

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 fn_bash=~/.bashrc
-fn_input=~/.inputrc
 
 if [ -f $fn_bash ]; then
     bakfn=${fn_bash}.bak
     mv $fn_bash $bakfn
 fi
 
-if [ -f $fn_input ]; then
-    bakfn=${fn_input}.bak
-    mv $fn_input $bakfn
-fi
-
 cp `dirname $0`/bashrc $fn_bash
-cp `dirname $0`/inputrc $fn_input
 
